89a279bfd1f0789404fee7511a96e91f6685333a
chiragbadhe <badhechirag633@gmail.com>
2023-08-25 20:08:12 +0530

    check

--- src/pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import { VmComponent } from "@/components/vm/VmComponent";
-
-export default function PolygonZkEVM() {
-  return (
-    <div>
-      <VmComponent src="mattlock.near/widget/zk-evm-lp" />
-    </div>
-  );
-}
